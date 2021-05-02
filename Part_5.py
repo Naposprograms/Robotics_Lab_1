b4b007af3c00fbb2d8cb914fd99a62ac3b368775
Napolitano <ganapolitano@indracompany.com>
2021-05-02 01:06:50 -0300

    Finished Part_5

--- Part_5.py
+++ Part_5.py
@@ -9,20 +9,18 @@ def run_program():
     print("\nRunning Part_5.py")
     flag_key_pressed = False
     upper_hsv_range = np.array([80, 255, 255])
-    lower_hsv_range = np.array([40, 5, 5])
+    lower_hsv_range = np.array([40, 70, 50])
     capture = cv2.VideoCapture(0)
-    print("\nPress ESC to exit.")
+
     while (not flag_key_pressed):
-    
         capture_ok, frame = capture.read()
         if capture_ok:
-            frame = cv2.imread(path)
             hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
             mask = cv2.inRange(hsv, lower_hsv_range, upper_hsv_range)
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             cv2.drawContours(frame, contours, -1, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.imshow("Contours", frame)
-        
+            cv2.imshow("Contours of green objects - Press ESC to exit.", frame)
+                
         if cv2.waitKey(1) & 0xFF == 27:
             flag_key_pressed = True
 
